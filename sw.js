7f13eabbf3470f1a84d993866372024083c922d9
ameesencial-sys <ame.esencial@gmail.com>
2026-09-25 13:50:05 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='envios-ame-v2';
-const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const C='envios-ame-v3';
+const FILES=['./','./index.html','./manifest.json','./camion-192.png','./camion-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(n=>n!==C).map(n=>caches.delete(n)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r;}).catch(()=>caches.match(e.request,{ignoreSearch:true})));});
